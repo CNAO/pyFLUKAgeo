427ffd377500f4dd91291ce34ce1e1d05acecf31
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2023-09-14 04:52:45 +0200

    testing grid.py and bug fixing

--- grid.py
+++ grid.py
@@ -25,6 +25,10 @@ import numpy as np
 
 class GRID:
     def __init__(self,Rmin,Rmax,NR,Tmin,Tmax,NT,Pmin,Pmax,NP):
+        '''
+        NR, NT, NP are number of points along grid;
+        therefore, the number of steps are NR-1, NT-1, NP-1
+        '''
         self.RRs=np.linspace(Rmin,Rmax,num=NR)
         self.TTs=np.linspace(Tmin,Tmax,num=NT)
         self.PPs=np.linspace(Pmin,Pmax,num=NP)
@@ -33,11 +37,11 @@ class GRID:
     def SymOneLayer(R,Tmax,NT,Pmax,NP):
         return GRID(R,R,1,-Tmax,Tmax,NT,-Pmax,Pmax,NP)
     
-    def CreateHive(self,dR=None,dT=None,dP=None):
+    def DefineHiveBoundaries(self,dR=None,dT=None,dP=None,lDebug=False):
         # get dR
         if (len(self.RRs)==1):
             if (dR is None):
-                write("dR NOT defined!")
+                print("dR NOT defined!")
                 exit(1)
         else:
             if (dR is None):
@@ -45,21 +49,37 @@ class GRID:
         # get dT
         if (len(self.TTs)==1):
             if (dT is None):
-                write("dT NOT defined!")
+                print("dT NOT defined!")
                 exit(1)
         else:
             if (dT is None):
-                dR=self.TTs[1]-self.TTs[0]
+                dT=self.TTs[1]-self.TTs[0]
         # get dP
         if (len(self.PPs)==1):
             if (dP is None):
-                write("dP NOT defined!")
+                print("dP NOT defined!")
                 exit(1)
         else:
             if (dP is None):
                 dP=self.PPs[1]-self.PPs[0]
+        if (lDebug):
+            print("self.RRs,dR:",self.RRs,dR)
+            print("self.TTs,dT:",self.TTs,dT)
+            print("self.PPs,dP:",self.PPs,dP)
         RRs=np.append(self.RRs-dR/2.,self.RRs[-1]+dR/2.)
         TTs=np.append(self.TTs-dT/2.,self.TTs[-1]+dT/2.)
         PPs=np.append(self.PPs-dP/2.,self.PPs[-1]+dP/2.)
         return RRs, TTs, PPs
 
+if ( __name__ == "__main__" ):
+    # perform some tests
+    R=500
+    Tmax=30
+    NT=10+1
+    Pmax=20
+    NP=20+1
+    myGrid=GRID.SymOneLayer(R,Tmax,NT,Pmax,NP)
+    RRs,TTs,PPs=myGrid.DefineHiveBoundaries(dR=100)
+    print(RRs)
+    print(TTs)
+    print(PPs)
