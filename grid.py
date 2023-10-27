aeadd933f27d7d47ed4e12711bb35879813cf755
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2023-10-27 13:21:21 +0200

    improving comments and interfaces. Adding some lDebug statements

--- grid.py
+++ grid.py
@@ -4,13 +4,13 @@
 #
 # This class describes the grid of points/orientations of crystals in the full
 #    CALO geometry. Coordinates/orientations move the centre of each crystal,
-#    defined at the origin of the FLUKA ref system of the geometry of the single
-#    crystal.
+#    defined at the origin of the FLUKA ref system of the single crystal, to the
+#    actual position/orientation.
 #
 # Reference frame (FLUKA-like):
 # - z-axis along beam;
 # - y-axis anti-gravitational;
-# - x-axis to get a right-hand ref sys.
+# - x-axis to get a right-handed ref sys.
 # 
 # The grid is described in spherical coordinates:
 # - r [cm];
@@ -24,20 +24,25 @@
 import numpy as np
 
 class GRID:
-    def __init__(self,Rmin,Rmax,NR,Tmin,Tmax,NT,Pmin,Pmax,NP):
+    def __init__(self,Rmin,Rmax,NR,Tmin,Tmax,NT,Pmin,Pmax,NP,lDebug=False):
         '''
         NR, NT, NP are number of points along grid;
-        therefore, the number of steps are NR-1, NT-1, NP-1
+        therefore, the number of steps are NR-1, NT-1, NP-1;
         '''
+        # np.linspace: num is number of points
         self.RRs=np.linspace(Rmin,Rmax,num=NR)
         self.TTs=np.linspace(Tmin,Tmax,num=NT)
         self.PPs=np.linspace(Pmin,Pmax,num=NP)
+        if (lDebug):
+            print("GRID.__init__(): self.RRs:",self.RRs)
+            print("GRID.__init__(): self.TTs:",self.TTs)
+            print("GRID.__init__(): self.PPs:",self.PPs)
 
     @staticmethod
-    def SymOneLayer(R,Tmax,NT,Pmax,NP):
-        return GRID(R,R,1,-Tmax,Tmax,NT,-Pmax,Pmax,NP)
+    def OneLayer(R,Tmax,NT,Pmax,NP,lDebug=True):
+        return GRID(R,R,1,-Tmax,Tmax,NT,-Pmax,Pmax,NP,lDebug=lDebug)
     
-    def DefineHiveBoundaries(self,dR=None,dT=None,dP=None,lDebug=False):
+    def DefineHiveBoundaries(self,lDebug=False,dR=None,dT=None,dP=None):
         # get dR
         if (len(self.RRs)==1):
             if (dR is None):
@@ -62,24 +67,22 @@ class GRID:
         else:
             if (dP is None):
                 dP=self.PPs[1]-self.PPs[0]
-        if (lDebug):
-            print("self.RRs,dR:",self.RRs,dR)
-            print("self.TTs,dT:",self.TTs,dT)
-            print("self.PPs,dP:",self.PPs,dP)
         RRs=np.append(self.RRs-dR/2.,self.RRs[-1]+dR/2.)
         TTs=np.append(self.TTs-dT/2.,self.TTs[-1]+dT/2.)
         PPs=np.append(self.PPs-dP/2.,self.PPs[-1]+dP/2.)
+        if (lDebug):
+            print("GRID.DefineHiveBoundaries(): RRs,dR:",RRs,dR)
+            print("GRID.DefineHiveBoundaries(): TTs,dT:",TTs,dT)
+            print("GRID.DefineHiveBoundaries(): PPs,dP:",PPs,dP)
         return RRs, TTs, PPs
 
 if ( __name__ == "__main__" ):
     # perform some tests
+    lDebug=True
     R=500
-    Tmax=30
-    NT=10+1
-    Pmax=20
-    NP=20+1
-    myGrid=GRID.SymOneLayer(R,Tmax,NT,Pmax,NP)
-    RRs,TTs,PPs=myGrid.DefineHiveBoundaries(dR=100)
-    print(RRs)
-    print(TTs)
-    print(PPs)
+    Tmax=27   # theta [degs] --> range: -Tmax:Tmax
+    NT=10     # number of steps (i.e. entities)
+    Pmax=20   # phi [degs] --> range: -Pmax:Pmax
+    NP=21     # number of steps (i.e. entities)
+    myGrid=GRID.OneLayer(R,Tmax,NT,Pmax,NP,lDebug=lDebug)
+    RRs,TTs,PPs=myGrid.DefineHiveBoundaries(lDebug=lDebug,dR=100)
