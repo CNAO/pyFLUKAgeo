d3f0aa11881667977bc786aed0dfe02e7b7e1656
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2023-12-13 10:35:52 +0100

    adding the Hive class in grid module

--- grid.py
+++ grid.py
@@ -2,11 +2,6 @@
 # A. Mereghetti, 2023/10/28
 # python version: >= 3.8.10;
 #
-# This class describes a grid of points/orientations onto which a prototype
-#    geometry should be cloned. Coordinates/orientations are meant to move the
-#    centre of the prototype (expected to be at the origin of the FLUKA ref
-#    system) to the actual position/orientation.
-#
 # Reference frame (FLUKA-like):
 # - z-axis along beam;
 # - y-axis anti-gravitational;
@@ -15,10 +10,10 @@
 import numpy as np
 import myMath
 
-class LOCATION:
+class Location:
     '''
-    a very simple class storing a position (3D array) and an orientation
-       (3D rotation matrix)
+    a very simple class storing a single position (3D array) and a single
+       orientation (3D rotation matrix)
     '''
     def __init__(self,myP=[0.,0.,0.],myW=myMath.UnitMat,myLab=""):
         self.P=myP
@@ -46,9 +41,15 @@ class LOCATION:
         buf=buf+" | label=\"%s\";"%(self.label)
         return buf
         
-class GRID:
+class Grid:
+    '''
+    This class describes a grid of points/orientations onto which a prototype
+       geometry should be cloned. Coordinates/orientations are meant to move the
+       centre of the prototype (expected to be at the origin of the FLUKA ref
+       system) to the actual position/orientation.
+    '''
     def __init__(self):
-        self.locs=[] # list of LOCATIONs
+        self.locs=[] # list of Locations
 
     def __len__(self):
         return len(self.locs)
@@ -68,13 +69,24 @@ class GRID:
         raise StopIteration
 
     def AddLoc(self,myP=[0.,0.,0.],myW=myMath.UnitMat,myLab=""):
-        self.locs.append(LOCATION(myP=myP,myW=myW,myLab=myLab))
+        self.locs.append(Location(myP=myP,myW=myW,myLab=myLab))
 
     def echo(self,myFmt="% .6E"):
         buf=""
         for myLoc in self.locs:
             buf=buf+myLoc.echo(myFmt=myFmt)+"\n"
         return buf
+
+    def ret(self,myWhat="point",iEl=-1):
+        if (myWhat.upper()=="POINT"):
+            return self.locs[iEl].P
+        elif (myWhat.upper()=="REF"):
+            return self.locs[iEl].W
+        elif (myWhat.upper()=="LAB"):
+            return self.locs[iEl].label
+        else:
+            print("Grid.ret(): unknown request %s!"%(myWhat))
+            exit(1)
 
     @staticmethod
     def SphericalShell(Rmin,Rmax,NR,Tmin,Tmax,NT,Pmin,Pmax,NP,\
@@ -86,12 +98,10 @@ class GRID:
         - r [cm];
         - theta [degs]: angle in yz-plane (positive when pointing towards y>0);
         - phi [degs]: angle in xz-plane (positive when pointing towards x>0).
+        The grid starts around the z-axis.
 
-        The grid is centred around the z-axis and symmetric in the two
-          directions, up/down (y>0/y<0), left/right (x>0/x<0).
-
-        NR, NT, NP are number of points along grid;
-        therefore, the number of steps are NR-1, NT-1, NP-1;
+        NR, NT, NP are number of points along grid (cell centers);
+          therefore, the number of steps are NR-1, NT-1, NP-1;
         '''
         # define unique shell values of radius and angles
         # NB: np.linspace: num is number of points
@@ -100,7 +110,7 @@ class GRID:
         PPs=np.linspace(Pmin,Pmax,num=NP)
         
         # create set of locations (coordinates and orientations)
-        newGrid=GRID()
+        newGrid=Grid()
         for tmpR in RRs:
             for tmpT in TTs:
                 for tmpP in PPs:
@@ -112,38 +122,100 @@ class GRID:
                     newGrid.AddLoc(myP=myP,myW=myW,myLab=myLab)
                     
         if (lDebug):
-            print("GRID.SphericalShell(): RRs:",RRs)
-            print("GRID.SphericalShell(): TTs:",TTs)
-            print("GRID.SphericalShell(): PPs:",PPs)
+            print("Grid.SphericalShell(): RRs:",RRs)
+            print("Grid.SphericalShell(): TTs:",TTs)
+            print("Grid.SphericalShell(): PPs:",PPs)
             print(newGrid.echo())
             
         return newGrid
     
     @staticmethod
     def SphericalShell_OneLayer(R,Tmax,NT,Pmax,NP,lDebug=False):
-        return GRID.SphericalShell(R,R,1,-Tmax,Tmax,NT,-Pmax,Pmax,NP,\
+        '''
+        Special case of a spherical shell instance, with:
+        - only 1 radial layer;
+        - symmetric angular ranges;
+        '''
+        return Grid.SphericalShell(R,R,1,-Tmax,Tmax,NT,-Pmax,Pmax,NP,\
                                    lDebug=lDebug)
 
-def DefHiveBoundaries_SphericalShell(Rmin,Rmax,dR,Tmin,Tmax,dT,Pmin,Pmax,dP):
+class Hive:
     '''
-    All input data in interface refer to the hive, not to the grid of objects
-       therein contained!
+    This class implements the coordinates of the hive for the grid.
+    The actual structure of the class depends on the type.
     '''
-    print("defining hive boundaries for a spherical shell:")
-    print("* R[cm]=[%g:%g:%g];"%(Rmin,dR,Rmax))
-    print("* theta[deg]=[%g:%g:%g];"%(Tmin,dT,Tmax))
-    print("* phi[deg]=[%g:%g:%g];"%(Pmin,dP,Pmax))
-    RRs=np.arange(Rmin,Rmax+dR,dR)
-    TTs=np.arange(Tmin,Tmax+dT,dT)
-    PPs=np.arange(Pmin,Pmax+dP,dP)
-    return RRs, TTs, PPs
-    
-def DefHiveBoundaries_SphericalShell_OneLayer(R,dR,Tmax,NT,Pmax,NP):
-    dT=2*Tmax/NT
-    dP=2*Pmax/NP
-    return DefHiveBoundaries_SphericalShell(R-dR/2,R+dR/2,dR,\
-                                            -Tmax-dT/2,Tmax+dT/2,dT,\
-                                            -Pmax-dP/2,Pmax+dP/2,dP)
+    def __init__(self,hType=None):
+        if (hType is not None):
+            if (hType.upper()!="SPHERE"):
+                print("Hive.__init__(): cannot create a hive of type %s!"%(\
+                    hType))
+                exit(1)
+        self.hType=hType
+
+    def ret(self,myWhat="all"):
+        if (self.hType.upper()=="SPHERE"):
+            if (myWhat.upper()=="RRS"):
+                return self.RRs
+            elif (myWhat.upper()=="TTS"):
+                return self.TTs
+            elif (myWhat.upper()=="PPS"):
+                return self.PPs
+            elif (myWhat.upper()=="ALL"):
+                return self.ret(myWhat="RRs"), \
+                       self.ret(myWhat="TTs"), \
+                       self.ret(myWhat="PPs")
+            else:
+                print("Hive.ret(): what to return? %s"%(myWhat))
+
+    @staticmethod
+    def SphericalHive(Rmin,Rmax,NR,Tmin,Tmax,NT,Pmin,Pmax,NP,lDebug=False):
+        '''
+        Method for creating a spherical hive - actually a spherical shell,
+          for which a sphere is a special case.
+
+        All the input info refer to the respective spherical grid of objects.
+          Please see the docstring of the Grid.SphericalShell() method.
+        The number of interfaces in the hive along r, theta and phy
+          are NR+1, NT+1 and NP+1
+        '''
+        newHive=Hive("SPHERE")
+        hdR=(Rmax-Rmin)
+        if (NR==1):
+            hRmax=Rmax-hdR/2.; hRmin=Rmin-hdR/2.
+        else:
+            hdR=hdR/(NR-1)
+            hRmax=Rmax+hdR; hRmin=Rmin-hdR
+        hdT=(Tmax-Tmin)
+        if (NT==1):
+            hTmax=Tmax-hdT/2.; hTmin=Tmin-hdT/2.
+        else:
+            hdT=hdT/(NT-1)
+            hTmax=Tmax+hdT; hTmin=Tmin-hdT
+        hdP=(Pmax-Pmin)
+        if (NP==1):
+            hPmax=Pmax-hdP/2.; hPmin=Pmin-hdP/2.
+        else:
+            hdP=hdP/(NP-1)
+            hPmax=Pmax+hdP; hPmin=Pmin-hdP
+        # define unique shell values of radius and angles
+        if (lDebug):
+            print("* R[cm]=[%g:%g:%g];"%(hRmin,hdR,hRmax))
+            print("* theta[deg]=[%g:%g:%g];"%(hTmin,hdT,hTmax))
+            print("* phi[deg]=[%g:%g:%g];"%(hPmin,hdP,hPmax))
+        # NB: np.linspace: num is number of points
+        newHive.RRs=np.linspace(hRmin,hRmax,num=NR+1)
+        newHive.TTs=np.linspace(hTmin,hTmax,num=NT+1)
+        newHive.PPs=np.linspace(hPmin,hPmax,num=NP+1)
+        return newHive
+
+    @staticmethod
+    def SphericalHive_OneLayer(R,dR,Tmax,NT,Pmax,NP,lDebug=False):
+    '''
+    Special case of a spherical hive, with:
+    - only 1 radial layer;
+    - symmetric angular ranges;
+    '''
+    return Hive.SphericalHive(R,R+dR,1,-Tmax,Tmax,NT,-Pmax,Pmax,NP,lDebug=lDebug)
 
 if ( __name__ == "__main__" ):
     # perform some tests
@@ -153,7 +225,7 @@ if ( __name__ == "__main__" ):
     NT=2      # number of steps (i.e. entities)
     Pmax=2    # phi [degs] --> range: -Pmax:Pmax
     NP=2      # number of steps (i.e. entities)
-    myGrid=GRID.SphericalShell_OneLayer(R,Tmax,NT,Pmax,NP,lDebug=lDebug)
+    myGrid=Grid.SphericalShell_OneLayer(R,Tmax,NT,Pmax,NP,lDebug=lDebug)
     # print(DefHiveBoundaries_SphericalShell_OneLayer(R,50,Tmax,NT,Pmax,NP))
     for loc in myGrid:
         print(loc.ret("Point"))
